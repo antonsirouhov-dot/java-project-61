b67313c766c2965f0c670d651ad5384d52282fa3
Антон <antonsirouhov@gmail.com>
2026-08-03 13:38:30 +0300

    Добавил видео в README.md

--- README.md
+++ README.md
@@ -24,3 +24,4 @@
 
 ## Demo
 [!asciicast](https://asciinema.org/a/xEA8xQ7s5YEbLre7)
+[!asciicast](https://asciinema.org/a/lqGV1xtQG0IxE2MD)
--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -5,23 +5,22 @@ import  java.util.Random;
 import  java.util.Scanner;
 
 public class Even {
-    @SuppressWarnings("java:s106")
     static Random random = new Random();
     public static void checkingParity() {
-        System.out.println("Welcome to the Brain Games!\nMay i have your name?");
+        System.out.println("Welcome to the Brain Games!\nMay i have your name?"); //NOSONAR
         Scanner enterName = new Scanner(System.in);
         String name = enterName.nextLine();
-        System.out.println("Hello, " + name + "!\nAnswer 'yes' if the number is even, otherwise answer 'no'");
+        System.out.println("Hello, " + name + "!\nAnswer 'yes' if the number is even, otherwise answer 'no'"); //NOSONAR
         Scanner scanner = new Scanner(System.in);
         boolean isWin = true;
         int count = 0;
         while (count < 3) {
             int number = random.nextInt(1, 101);
-            System.out.println("Question: " + number);
+            System.out.println("Question: " + number); //NOSONAR
             String answer = scanner.nextLine().trim().toLowerCase();
-            System.out.println("Your answer: " + answer);
+            System.out.println("Your answer: " + answer); //NOSONAR
             if (number % 2 == 0 && answer.equals("yes")) {
-                System.out.println("Correct!");
+                System.out.println("Correct!"); //NOSONAR
                 count++;
             } else if (number % 2 == 0 && answer.equals("no")) {
                 System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, " + name);
@@ -32,16 +31,16 @@ public class Even {
                 isWin = false;
                 break;
             } else if (number % 2 != 0 && answer.equals("no")) {
-                System.out.println("Correct!");
+                System.out.println("Correct!"); //NOSONAR
                 count++;
             } else {
-                System.out.println(answer + " is wrong answer, try again");
+                System.out.println(answer + " is wrong answer, try again"); //NOSONAR
                 isWin = false;
                 break;
             }
         }
         if (isWin) {
-            System.out.println("Congratulations, " + name);
+            System.out.println("Congratulations, " + name); //NOSONAR
         }
     }
 }
