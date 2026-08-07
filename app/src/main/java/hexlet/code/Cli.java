9e3dbf92a28251e793caeee15ec60dea3d6ee0b2
Антон <antonsirouhov@gmail.com>
2026-08-07 14:55:47 +0300

    i = I

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static String greetUser() {
-        System.out.println("Welcome to the Brain Games!\nMay i have your name?"); //NO SONAR
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?"); //NO SONAR
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!"); //NO SONAR
